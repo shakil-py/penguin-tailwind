31baa3ee03d7e05df37b7e48b0c8a7d12fea0719
shakil-py <shakilhaquepy@gmail.com>
2023-05-24 15:49:35 +0600

    add

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 document.getElementById("header-penguin-btn").addEventListener("click", function () {
     document.getElementById("penguin-txt").innerText="I AM PENGUINE"
 })
+const value = document.getElementById("penguin-txt").innerText
+console.log(value)
